8146b31082b1bc146c10e0753e36aec53a733858
MohitSalvi28 <75218204+MohitSalvi28@users.noreply.github.com>
2022-09-06 22:48:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express =require('express');
+const cors=require('cors');
+
+const authRoutes =require("./routes/auth.js")
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+require('dotenv').config();
+
+
+
+// app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/',(req,res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.send('Hello, World!');
+});
+
+app.use('/auth',authRoutes);
+
+app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+
